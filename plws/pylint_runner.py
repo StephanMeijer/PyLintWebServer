ae7668afe08f1a413dd020562705499b87c7dd00
Ghostbird <gijsbertth@gmail.com>
2016-06-10 17:36:27 +0200

    Correct relative import

--- plws/pylint_runner.py
+++ plws/pylint_runner.py
@@ -9,7 +9,8 @@ class WritableObject(object):
     def read(self):
         return self.content
 
-def lint_to_text(path):
+def lint_to_text(paths):
     pylint_output = WritableObject()
-    lint.Run([path], reporter=TextReporter(pylint_output), exit=False)
+    for path in paths:
+        lint.Run(path, reporter=TextReporter(pylint_output), exit=False)
     return "".join(pylint_output.read())
